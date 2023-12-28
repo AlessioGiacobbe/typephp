61823110ab4b6ecbe4a77591b08fcfaa75673c99
tianfenghan <rango@swoole.com>
2023-12-28 12:59:04 +0800

    init 3

---
+++ web/include/lang.php
@@ -0,0 +1,18 @@
+<select id="selectLanguage">
+    <?php if (false): ?>
+        <option value="language-html">HTML</option>
+        <option value="language-javascript">JavaScript</option>
+        <option value="language-python">Python</option>
+        <option value="language-java">Java</option>
+        <option value="language-csharp">C#</option>
+        <option value="language-php">PHP</option>
+        <option value="language-cpp">C++</option>
+        <option value="language-typescript">TypeScript</option>
+        <option value="language-ruby">Ruby</option>
+        <option value="language-swift">Swift</option>
+        <option value="language-kotlin">Kotlin</option>
+    <?php else: ?>
+        <option value="language-python">Python</option>
+        <option value="language-php">PHP</option>
+    <?php endif; ?>
+</select>
