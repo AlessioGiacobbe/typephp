034c09c8a2107fac54421a754e8d3283f6f9a058
tianfenghan <rango@swoole.com>
2023-12-28 20:05:04 +0800

    op

--- py2php.php
+++ py2php.php
@@ -1,4 +1,17 @@
 <?php
 $file = __DIR__ . '/cases/' . $_GET['file'];
 
-echo shell_exec('php conv.php ' . $file . ' web');
+$op = empty($_GET['op']) ? 'conv' : $_GET['op'];
+
+switch ($op) {
+    case 'conv':
+        echo shell_exec('php conv.php ' . $file . ' web');
+        break;
+    case 'dump':
+        echo shell_exec('python dump.py ' . $file);
+        break;
+    default:
+        break;
+}
+
+
