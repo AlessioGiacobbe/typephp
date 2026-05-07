08890ea6303b1d85dd1832b67422614aa6331bc4
rango <rango@swoole.com>
2026-05-07 18:37:49 +0800

    style(config): 更新PHP代码样式配置
    - 将单引号设置为false以禁用单引号强制规则

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -53,7 +53,7 @@ return (new PhpCsFixer\Config())
         'single_line_comment_style'                        => ['comment_types' => []],
         'single_line_comment_spacing'                      => false,
         'single_line_empty_body'                           => false,
-        'single_quote'                                     => true,
+        'single_quote'                                     => false,
         'standardize_increment'                            => false,
         'standardize_not_equals'                           => true,
         'yoda_style'                                       => ['always_move_variable' => false, 'equal' => false, 'identical' => false],
