25aeff51a7f13ddd77cfb5bd893c320a7923d424
tianfenghan <rango@swoole.com>
2026-06-20 22:13:28 +0800

    refactor(generator): 简化类对象处理器初始化逻辑
    - 移除手动初始化 zend_object_handlers 的冗余代码
    - 直接使用预定义的 php_aot_object_handlers 全局变量
    - 减少代码行数并提高可读性
    - 保持相同的功能实现

--- src/gen_stub.php
+++ src/gen_stub.php
@@ -3781,10 +3781,7 @@ class ClassInfo {
             $code .= $php80CondEnd;
         }
 
-        $code .= "\n\tstatic zend_object_handlers class_object_handlers;";
-        $code .= "\n\tphp_aot_init_object_handlers(&class_object_handlers);";
-        $code .= "\n\tclass_entry->default_object_handlers = &class_object_handlers;";
-        $code .= "\n";
+        $code .= "\n\tclass_entry->default_object_handlers = &php_aot_object_handlers;\n";
 
         $code .= "\n\treturn class_entry;\n";
 
