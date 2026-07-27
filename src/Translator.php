d9bf4b472b17cde21ef6dcaab6c61f6971e5b58b
tianfenghan <rango@swoole.com>
2026-07-27 18:28:21 +0800

    chore(version): bump version to 0.4.3
    - Update VERSION constant from 0.4.2 to 0.4.3 in Translator class

--- src/Translator.php
+++ src/Translator.php
@@ -58,7 +58,7 @@ class Translator extends Preprocessor
     use ResourceCompilationTrait;
     use ClassConstantValueTrait;
 
-    public const string VERSION = '0.4.2';
+    public const string VERSION = '0.4.3';
     public const string APP_NAME = 'TypePHP Compiler (AOT)';
 
     protected string $targetName = 'app';
